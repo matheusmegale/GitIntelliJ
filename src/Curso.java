854a2e838ad00e9153201e7a7874472e5adf02d4
Matheus Megale <matheusmegale5@gmail.com>
2024-12-07 20:08:16 -0300

    Classe Curso criada

---
+++ src/Curso.java
@@ -0,0 +1,27 @@
+public class Curso {
+    private String nome;
+    private int codigo;
+
+    public Curso(String nome, int codigo) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+}
+
+
