d9a8d260487de2a280dd071b377c03a2255ffc51
Matheus Megale <matheusmegale5@gmail.com>
2024-12-11 15:27:23 -0300

    Criei a classe Professor

---
+++ src/Professor.java
@@ -0,0 +1,25 @@
+public class Professor {
+    private String nome;
+    private String disciplina;
+
+    public Professor(String nome, String disciplina) {
+        this.nome = nome;
+        this.disciplina = disciplina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+}
